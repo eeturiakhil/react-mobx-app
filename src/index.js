dc39ebf2fe97bf068a6c66a05d25ae8908c1dfb1
Akhil Kumar <eeturiakhil@gmail.com>
2021-08-01 14:15:32 +0530

    Mobx changes initial commit 01-08-2021

--- src/index.js
+++ src/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// import { useStrict } from "mobx";
+import { action, autorun, configure, observable } from "mobx";
+
+// useStrict(true);
+configure({ enforceActions: "observed" });
+
+class ShoppingCart {
+  @observable
+  items = [];
+
+  @action
+  add(item) {
+    this.items.push(item);
+  }
+}
+
+const shoppinCart = new ShoppingCart();
+
+autorun(() => {
+  console.log(shoppinCart.items.length);
+});
+
+shoppinCart.add({ id: 1, name: "Computer", price: 3000 });
+shoppinCart.add({ id: 2, name: "Mouse", price: 30 });
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
